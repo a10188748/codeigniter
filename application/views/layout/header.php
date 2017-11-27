60c0a641f6100faaa067659a56ba9339a0b73503
GAMANIA\seanyeh <a29543456@gmail.com>
2017-11-27 19:12:44 +0800

    2

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -1,1 +1,3 @@
-<div style="height:100px; width:100%; border:1px solid #000;"> Your Website Header Section </div>
+<div style="height:100px; width:100%; border:1px solid #000;">
+	<a href="/home" title="天堂M" style="float:left; background-image: url('../../public/images/001.jpg');">天堂M</a>
+</div>
