a43921cc30163c6b045d2e9d7aed1120c740779b
GAMANIA\seanyeh <a29543456@gmail.com>
2017-11-27 17:55:18 +0800

    2

---
+++ application/views/layout/front.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>StepBlogging.COM Layouts</title>
+    </head>
+    <body>
+    <?php 
+        if($header) echo $header ;
+        if($left)   echo $left ;
+        if($middle) echo $middle ;
+        if($footer) echo $footer ;
+    ?>
+    </body>
+</html>
---
+++ application/views/layout/left.php
@@ -0,0 +1,2 @@
+<div style="clear:both;"></div>
+<div style="height:500px; width:200px; border:2px solid #000; float:left;">  Left Area</div>
